ca41fdcd5183cd36063809be267fec0a2d59c046
Ahalya. R. S <122439393+ahalyars@users.noreply.github.com>
2024-05-17 12:33:55 +0530

    Delete 17 MAY 2024 directory

--- 17 MAY 2024/absalute.java
+++
@@ -1,8 +0,0 @@
-public class absalute {
-    public static void main(String[] args) {
-        int x=1;
-        int y=-1;
-        System.err.println(Math.abs(x));
-        System.err.println(Math.abs(y));
-    }
-}
